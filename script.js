1de8bbe844c65d03921f9b0561e1e9d429d75c2f
daniel-jedrasiewicz <jedrasiewicz.daniel@gmail.com>
2023-01-16 14:38:32 +0100

    add popup and todo item edit

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let errorInfo
 let addBtn
 let ulList
 
+let popup
+let popupInfo
+let todoToEdit
+let popupInput
+let popupAddBtn
+let popopCloseBtn
+
+
 const main = () => {
     prepareDOMEelements()
     prepareDOMEvents()
@@ -13,10 +21,19 @@ todoInput = document.querySelector('.todo-input')
 errorInfo = document.querySelector('.error-info')
 addBtn = document.querySelector('.btn-add')
 ulList = document.querySelector('.todolist ul')
+
+popup = document.querySelector('.popup')
+popupInfo = document.querySelector('.popup-info')
+popupInput= document.querySelector('.popup-input')
+popupAddBtn = document.querySelector('.accept')
+popupCloseBtn = document.querySelector('.cancel')
 }
 
 const prepareDOMEvents = () => {
     addBtn.addEventListener('click', addNewTodo)
+    ulList.addEventListener('click', checkClick)
+    popupCloseBtn.addEventListener('click', closePopup)
+    popupAddBtn.addEventListener('click', changeTodoText)
 
 }
 
@@ -54,6 +71,40 @@ const createToolsArea = (newTodo) => {
 
     toolsPanel.append(completeBtn,editBtn,deleteBtn)
 }
+
+const checkClick = e => {
+    if(e.target.matches('.complete')) {
+       e.target.closest('li').classList.toggle('completed')
+       e.target.classList.toggle('completed')
+    } else if (e.target.matches('.edit')) {
+        editTodo(e)
+    } else if (e.target.matches('.delete')) {
+        console.log('delete')
+    }
+}
+
+const editTodo = e => {
+    todoToEdit = e.target.closest('li')
+    popupInput.value = todoToEdit.firstChild.textContent
+    popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+    popup.style.display = 'none'
+    popupInfo.textContent = ''
+}
+
+const changeTodoText = () => {
+    if(popupInput.value !== "") {
+        todoToEdit.firstChild.textContent = popupInput.value
+        popup.style.display = 'none'
+        popupInfo.textContent = ''
+
+    } else {
+        popupInfo.textContent = 'Musisz wpisać treść !'
+    }
+}
+
 
 
 document.addEventListener('DOMContentLoaded', main)
